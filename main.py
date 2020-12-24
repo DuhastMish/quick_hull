f7c9beead11b1bb6ce1f66194e69f1ca8e872dcd
fantoff2010@yandex.ru <fantoff2010@yandex.ru>
2020-12-24 21:44:44 +0300

    Added dividing points into sets relative to a straight line

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from pathlib import Path
 from typing import List, Tuple
-
+import numpy as np
 DATA_PATH = Path('data.txt')
 
 
@@ -10,25 +11,57 @@ def read_data_file() -> List:
     return points_list
 
 
-def get_left_and_right_points(points_list: List) -> Tuple[Tuple[int, int], Tuple[int, int]]:
-    minimal_x = minimal_y = float('inf')
-    maximal_x = maximal_y = 0
-    for point in points_list:
-        coord_x, coord_y = point
-        if coord_x < minimal_x:
-            minimal_x = coord_x
-            minimal_y = coord_y
-        elif coord_x > maximal_y:
-            maximal_x = coord_x
-            maximal_y = coord_y
+def determine_position_from_line(line_point_left: Tuple,
+                                 line_point_right: Tuple,
+                                 point: Tuple):
+    lx, ly = line_point_left
+    rx, ry = line_point_right
+    px, py = point
+    return (px-lx)*(ry-ly) - (py-ly)*(rx-lx)
 
-    return ((int(minimal_x), int(minimal_y)), (maximal_x, maximal_y))
+
+def build_quick_hull(points: List[Tuple]):
+    def get_left_and_right_points(points_list: List) -> Tuple[Tuple[int, int], Tuple[int, int]]:
+        minimal_x = minimal_y = float('inf')
+        maximal_x = maximal_y = 0
+        for point in points_list:
+            coord_x, coord_y = point
+            if coord_x < minimal_x:
+                minimal_x = coord_x
+                minimal_y = coord_y
+            if coord_x > maximal_y:
+                maximal_x = coord_x
+                maximal_y = coord_y
+
+        return ((int(minimal_x), int(minimal_y)), (maximal_x, maximal_y))
+
+    upper_set = []
+    bottom_set = []
+    sorted_points = points.sort(key=lambda x: (x[0], x[1]))
+    left_point, right_point = get_left_and_right_points(points)
+    x, y = left_point
+    upper_left_point = (x, y+1)
+    upper_side_of_line = determine_position_from_line(left_point, right_point, upper_left_point)
+
+    for point in points:
+        if point not in [left_point, right_point]:
+            side_of_line = determine_position_from_line(left_point, right_point, point)
+            if np.sign(side_of_line) == np.sign(upper_side_of_line):
+                upper_set.append(point)
+            else:
+                bottom_set.append(point)
+
+    return upper_set, bottom_set
 
 
 def quick_hull():
     points = read_data_file()
-    left_point, right_point = get_left_and_right_points(points)
-    import pdb; pdb.set_trace()
+    start_time = datetime.now()
+
+    build_quick_hull(points)
+
+    end_time = datetime.now()
+    print(f"Estimated time: {end_time - start_time} msc.")
 
 
 if __name__ == "__main__":
