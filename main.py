d1635f3730f9fc505b2277ca7d2b688f06a45e8b
fantoff2010@yandex.ru <fantoff2010@yandex.ru>
2020-12-25 22:47:04 +0300

    ADded sorting for result points. Also added checking for null list of points

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from math import sqrt
+from math import sqrt, atan2
 from pathlib import Path
 from typing import List, Tuple
 
@@ -55,44 +55,41 @@ def build_quick_hull(points: List[Tuple]):
         return ((int(minimal_x), int(minimal_y)), (maximal_x, maximal_y))
 
     def find_hull_point(points: List[Tuple], left_point: Tuple, right_point: Tuple) -> List:
-        hulls = []
-        default_distance = float('-inf')
-        line_length = distance_between_points(left_point, right_point)
-        for point in points:
-            point_orientation = determine_position_from_line(left_point, right_point, point)
-            dist_point_to_line = distance_from_point_to_line(point_orientation, line_length)
-            default_distance = max(dist_point_to_line, default_distance)
-            if default_distance == dist_point_to_line:
-                hull_point = point
-            pass
-        hulls.append(hull_point)
-        upper_set = []
-        buttom_set = []
-        upper_point_orientation = determine_position_from_line(left_point, hull_point, right_point)
-        bottom_point_orientation = determine_position_from_line(hull_point, right_point, left_point)
-        for point in points:
-            if point not in hulls:
-                if not sign(determine_position_from_line(left_point, hull_point, point),
-                            upper_point_orientation):
-                    upper_set.append(point)
-                if not sign(determine_position_from_line(hull_point, right_point, point),
-                            bottom_point_orientation):
-                    buttom_set.append(point)
-        if upper_set:
-            up_set = find_hull_point(upper_set, left_point, hull_point)
-        else:
-            up_set = []
-        if bottom_set:
-            bot_set = find_hull_point(buttom_set, hull_point, right_point)
-        else:
-            bot_set = []
+        if points:
+            hulls = []
+            default_distance = float('-inf')
+            line_length = distance_between_points(left_point, right_point)
+            for point in points:
+                point_orientation = determine_position_from_line(left_point, right_point, point)
+                dist_point_to_line = distance_from_point_to_line(point_orientation, line_length)
+                default_distance = max(dist_point_to_line, default_distance)
+                if default_distance == dist_point_to_line:
+                    hull_point = point
+            hulls.append(hull_point)
 
-        return hulls + bot_set + up_set
+            upper_set = []
+            bottom_set = []
+            upper_point_orientation = determine_position_from_line(left_point, hull_point, right_point)
+            bottom_point_orientation = determine_position_from_line(hull_point, right_point, left_point)
+            for point in points:
+                if point not in hulls:
+                    if not sign(determine_position_from_line(left_point, hull_point, point),
+                                upper_point_orientation):
+                        upper_set.append(point)
+                    if not sign(determine_position_from_line(hull_point, right_point, point),
+                                bottom_point_orientation):
+                        bottom_set.append(point)
+            up_set = find_hull_point(upper_set, left_point, hull_point)
+            bot_set = find_hull_point(bottom_set, hull_point, right_point)
+
+            return hulls + bot_set + up_set
+        else:
+            return []
 
     upper_set = []
     bottom_set = []
     points.sort(key=lambda x: (x[0], x[1]))
-    left_point, right_point = get_left_and_right_points(points)
+    left_point, right_point = points[0], points[-1]
     x, y = left_point
     upper_left_point = (x, y+1)
     upper_side_of_line = determine_position_from_line(left_point, right_point, upper_left_point)
@@ -134,10 +131,19 @@ def quick_hull():
     result.extend(upper_set)
     result.extend(bottom_set)
     result.append(right_point)
+    sum_x = sum_y = 0
+    for point in result:
+        x, y = point
+        sum_x += x
+        sum_y += y
+    sort_x, sort_y = sum_x/len(result), sum_y/len(result)
+
+    result.sort(key=lambda x: (atan2(x[1]-sort_y, x[0]-sort_x)))
     print("Result:")
     for res_index, res_point in enumerate(result):
         try:
-            print(f"{indexies[res_point]} - {indexies[result[res_index + 1]]}")
+            if res_point != result[res_index + 1]:
+                print(f"{indexies[res_point]} - {indexies[result[res_index + 1]]}")
         except IndexError:
             print(f"{indexies[res_point]} - {indexies[result[0]]}")
 
